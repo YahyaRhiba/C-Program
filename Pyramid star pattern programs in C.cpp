762a2b5782d94df517383e344c5b55e19ddc8242
yahya rhiba <76631430+YahyaRhiba@users.noreply.github.com>
2021-01-13 00:27:35 +0100

    Create Pyramid star pattern programs in C.cpp

---
+++ Pyramid star pattern programs in C.cpp
@@ -0,0 +1,17 @@
+#include<conio.h>
+#include<stdio.h>
+main()
+{
+ int n,i,j;
+ printf("Nombres de lignes :  ");
+ scanf("%d",&n);
+ for (i=0;i<n;i++)
+   {
+    for (j=0;j<n-i-1;j++)
+        printf(" ");
+    for (j=0;j<2*i+1;j++)
+        printf("*");
+    printf("\n");
+   }
+  getch();
+}
