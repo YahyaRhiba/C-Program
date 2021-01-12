3b2047e589860d5f39cadeb76fc7b62818ea8e6b
yahya rhiba <76631430+YahyaRhiba@users.noreply.github.com>
2021-01-13 00:32:08 +0100

    Create Half Pyramid of Numbers.cpp

---
+++ Half Pyramid of Numbers.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h> 
+main() {
+   int i, j, line;
+   printf("Enter the number of line: ");
+   scanf("%d", &line);
+   for (i = 1; i <= line; ++i) {
+      for (j = 1; j <= i; ++j) {
+         printf("%d ", j);
+      }
+      printf("\n");
+   }
+   getch();
+}
